075b47da43c5392d8ae3989bb9498db68d66267b
Mike Ulanoski <43041991+mikeulanoski@users.noreply.github.com>
2018-10-23 15:02:34 -0400

    Initial upload of project files.

---
+++ script.js
@@ -0,0 +1,147 @@
+
+var btn1 = document.getElementById ('submit1')
+var txt1 = document.getElementById ("freelancetext1")
+    btn1.addEventListener('click', updateBtn1);
+        function updateBtn1()
+            {if (btn1.value === 'Click to view Freelance Engineer description.') 
+                {btn1.value = 'Click to close Freelance Engineer description.';
+                txt1.textContent = 'Seeking new opportunities in both contract & direct roles.';} 
+            else 
+                {btn1.value = 'Click to view Freelance Engineer description.';
+                txt1.textContent = '';}
+        }
+
+var btn2 = document.getElementById('submit2')
+var txt2 = document.getElementById("freelancetext2")
+    btn2.addEventListener('click', updateBtn2);
+        function updateBtn2() 
+            {if (btn2.value === 'Click to view Engineer IV job description') 
+                {btn2.value = 'Click to close Engineer IV job description';
+                txt2.textContent = 'As a member of the new product high horsepower'+ 
+                ' Diesel engine validation team, used a 7 step (similar to the DMAIC process)'+
+                ' process to resolve design issues encountered during new product diesel engine'+
+                ' testing (both rig and field testing).  When FIRG issues (FIRG - Failure Incidence'+
+                'Review Reporting Group) were uncovered, it was my job to contain the problem, determine'+
+                'root cause (using FTA - Fault Tree Analysis), find the solution, implement the corrective'+
+                ' action, and monitor the correction. Conducted, periodic design reviews on all issues. '+
+                'Oversaw ANSYS FEA analysis, preformed hand stress calculations, and practiced GD&T '+
+                '(Geometric Dimensioning and Tolerancing), on all new '+
+                'designs. Worked with Metrology department/Measurements lab to address'+ 
+                'part launch non-conformance issues and failed part situations. Understand,'+ 
+                'plan, and execute technical activities in accordance with ESW - '+
+                'Engineering Standard Work, FMEAs (Failure Mode & Effect Analysis) and '+
+                'specific Customer requirements. Conducted research that test and analyze '+
+                'the feasibility, design, operation and performance of equipment, '+
+                'components and systems.';}
+            else 
+                {btn2.value = 'Click to view Engineer IV job description';
+                txt2.textContent = '';}
+        }
+
+var btn3 = document.getElementById ('submit3')
+var txt3 = document.getElementById ("freelancetext3")
+    btn3.addEventListener('click', updateBtn3);
+        function updateBtn3() 
+            {if (btn3.value === 'Click to view Quality Process Improvement Engineer job description') 
+                {btn3.value = 'Click to close Quality Process Improvement Engineer job description';
+                txt3.textContent = 'Managed process quality improvement projects on the Escape vehicle'+
+                'line to reduce warranty and in plant process issues. Apply the BSAQ'+
+                '(Balanced Single Agenda for Quality) information to create quality'+ 
+                'projects, use Quality Leadership System data (Reduce large amounts '+
+                'of data to determine process issues) to use data reduction to detect '+
+                'process quality related issues. As a part of the Handles Locks, & '+
+                'Mechanisms variability reduction team, administer projects that '+
+                'reduce process related issues and increase overall quality of the '+
+                'HLM system.';}
+            else 
+                {btn3.value = 'Click to view Quality Process Improvement Engineer job description';
+                txt3.textContent = '';}
+            }
+var btn4 = document.getElementById ('submit4')
+var txt4 = document.getElementById ("freelancetext4")
+    btn4.addEventListener('click', updateBtn4);
+        function updateBtn4() 
+            {if (btn4.value === 'Click to view Service Engineer job description') 
+                {btn4.value = 'Click to close Service Engineer job description';
+                txt4.textContent = 'Assisted in the development and introduction of a web based '+
+                'Diesel Engine problem diagnosis software program. This program enables the average '+
+                'diesel mechanic to properly diagnose all engine issues encountered in the field. '+
+                'Engaged in software validation, script writing, data gathering/reduction, and '+
+                'customer contact to improve the overall effectiveness of the program.';}
+            else 
+                {btn4.value = 'Click to view Service Engineer job description';
+                txt4.textContent = '';}
+            }
+
+var btn5 = document.getElementById ('submit5')
+var txt5 = document.getElementById ("freelancetext5")
+    btn5.addEventListener('click', updateBtn5);
+        function updateBtn5() 
+            {if (btn5.value === 'Click to view Project Engineer job description') 
+                {btn5.value = 'Click to close Project Engineer job description';
+                txt5.textContent = 'Managed the valve train design of the 19 Liter Diesel engine '+
+                'program (in a union environment), accountable for developing design change solutions '+
+                '(this included working with vendors) for cost, and warranty reduction projects (design, '+
+                'test, and introduction). Implemented FMEAs (Failure Mode & Effect Analysis) & ESW '+
+                '(Engineering Standard Work) for assigned programs, developed test plans for assigned '+
+                'projects and preformed trial runs on new parts to assure smooth product rollout. '+
+                'Worked with Metrology departments at two separate companies on two continents to '+
+                'develop fixturing for new parts. Extensively worked with CMM (Coordinate Measuring '+
+                'Machine) operators to assure new parts adhered to drawing requirements. Developed '+
+                'new cost reduced parts for existing systems (using Cummins design (Engineering Standard '+
+                'Work) and test standards), and originated new part salvage repairs and coordinate wavers '+
+                'in order to support production.';}
+            else 
+                {btn5.value = 'Click to view Project Engineer job description';
+                txt5.textContent = '';}
+            }   
+var btn6 = document.getElementById ('submit6')
+var txt6 = document.getElementById ("freelancetext6")
+    btn6.addEventListener('click', updateBtn6);
+        function updateBtn6() 
+            {if (btn6.value === 'Click to view Engineer job description') 
+                {btn6.value = 'Click to close Engineer job description';
+                txt6.textContent = 'Established and edited SOPs - Standard Operating Procedures for the U.S. '+
+                'FDA 510(k) (Medical Device Submissions Requirements) approval of the Proton Beam Therapy system '+
+                'for the treatment of cancer at the Indiana University Cyclotron Facility.';}
+            else 
+                {btn6.value = 'Click to view Engineer job description';
+                txt6.textContent = '';}
+            }
+
+var btn7 = document.getElementById ('submit7')
+var txt7 = document.getElementById ("freelancetext7")
+    btn7.addEventListener('click', updateBtn7);
+        function updateBtn7() 
+            {if (btn7.value === 'Click to view Senior Chassis Design Engineer job description') 
+                {btn7.value = 'Click to close Senior Chassis Design Engineer job description';
+                txt7.textContent = 'Managed chassis systems (Brakes, Suspension, Tires, Frame - in both in '+
+                'house and vendor designs) design programs for Fords over 8500 lb.'+
+                '(Super Duty) truck line in a union environment. Accountable for new '+
+                'product introduction (NPI), design resolution of manufacturing issues, '+
+                'managed collaborative supplier relationships, and implementation of '+
+                'product design changes to lower warranty costs. Black-Belt who employed '+
+                'Six Sigma tools to reduce or eliminate warranty costs. Vigilant with '+
+                'Consumer Hot-Line affairs, and Field Support Personnel to achieve rapid '+
+                'resolution of product safety issues. Worked with onsite Metrology '+
+                'personnel to determine acceptance of out of print parts and assemblies. '+
+                'Design Group Representative in plant process variability reduction teams '+
+                'to recognize emerging fabrication related issues and provide rapid '+
+                'response counter measures. Implemented GD&T (Geometric Dimensioning '+
+                'and Tolerancing), on all new and current designs. Executed multiple '+
+                'simultaneous projects involving warranty reduction, design validation, '+
+                'process modification, new product introduction, PPAP, and quality '+
+                'improvement. Achieved project completions on time and according to '+
+                'plan. Initiated and approved 8Ds on problem parts/issues. As responsible '+
+                'engineer for the suspension system, warranty costs were reduced from '+
+                'being in the top ten to being virtually nonexistent. Attained $150,000 '+
+                'warranty reduction requirement per year goals using Six-Sigma tools to '+
+                'analyze problems and resolve production issues. Plant Chassis Launch '+
+               'Team Engineer for multiple model year vehicle launches at Kentucky '+
+                'Truck and Mexico City (Cuautitlan Assembly Plant). Monitored multiple vehicle '+
+                'characteristics to evaluate aspects of finished vehicle dynamic traits '+
+                'and monitor for negative trends and emerging problems.';}
+            else 
+                {btn7.value = 'Click to view Senior Chassis Design Engineer job description';
+                txt7.textContent = " ";}
+            }
